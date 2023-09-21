77d32763e2394c6d45d326cba3b04c2dc098ed6a
Anderson-23 <MichelAnderson.Silva23@gmail.com>
2023-09-21 20:50:42 -0300

    pulçar

--- treino2/index.js
+++ treino2/index.js
@@ -1,11 +1,17 @@
-document.addEventListener
 
-function pular(){
-
-    player.addEventListener("keydown",)
+const player = document.querySelector('.person');
 
 
+ 
+const pular= () => {
+ player.classList.add('pula');
 
+    setTimeout(() =>{
 
+        player.classList.remove('pula');
+
+    },800);
 
 }
+ 
+ document.addEventListener('keydown', pular)
